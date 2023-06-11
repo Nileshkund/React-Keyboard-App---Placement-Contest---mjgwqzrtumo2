33d56b4e684456b3924390c06c9137cf2354bbfb
NILESH KUND <111678618+Nileshkund@users.noreply.github.com>
2023-06-11 19:38:53 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,52 @@
+// import "../styles/App.css";
+// import React, { useState, useEffect } from "react";
+
+// const keys = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
+
+// const App = () => {
+
+//   return (
+//     <div className="keyboard">
+//       <div className="preview"></div>
+//       <div>
+//         {keys.map((key) => (
+//           <button key={key} id={key === " " ? `key-space` : `key-${key}`}>
+//             {key === " " ? "Space" : key.toUpperCase()}
+//           </button>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default App;
+
 import "../styles/App.css";
 import React, { useState, useEffect } from "react";
 
 const keys = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
 
 const App = () => {
+  const [input,setInput] = useState("");
+  const [quote,setQuote] = useState("");
+
+  useEffect(()=>{
+    if (input === "forty two") {
+      fetch("https://api.quotable.io/random")
+      .then(res=>res.json())
+      .then(data=>setQuote(data.content))
+    }else{
+      setQuote("");
+    }
+  },[input])
 
   return (
     <div className="keyboard">
-      <div className="preview"></div>
+      <div className="preview">{input}</div>
+      {!!quote && <div className="quote">{quote}</div>}
       <div>
         {keys.map((key) => (
-          <button key={key} id={key === " " ? `key-space` : `key-${key}`}>
+          <button key={key} id={key === " " ? `key-space` : `key-${key}`} onClick={()=>{setInput(prev=>prev + key)}}>
             {key === " " ? "Space" : key.toUpperCase()}
           </button>
         ))}
